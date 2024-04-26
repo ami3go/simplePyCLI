c67901f598a0b050d25ba296efe961b1674f97ba
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2024-04-26 21:40:17 +0300

    add empty command check modified help print

--- src/simplePyCLI.py
+++ src/simplePyCLI.py
@@ -7,32 +7,35 @@ class simplePyCLI:
         self.commands[command] = (action, max_params, description)  # Store the action and maximum parameters for the command
         # print(self.commands)
     def process_command(self, cmd_with_params):
-        cmd, *params = cmd_with_params.split()
-        in_var = cmd_with_params.split()
-        if cmd in self.commands:
-            action, max_params, help = self.commands[cmd]  # Retrieve the action and maximum parameters for the command
+        if cmd_with_params !="":
+            cmd, *params = cmd_with_params.split()
+            in_var = cmd_with_params.split()
+            if cmd in self.commands:
+                action, max_params, help = self.commands[cmd]  # Retrieve the action and maximum parameters for the command
 
-            #print(in_var, "given:", len(params), "max:", max_params)
+                #print(in_var, "given:", len(params), "max:", max_params)
 
-            if len(params) > max_params:
-                print(f'ERROR, {self.cursor}{in_var} -> Too many parameters. Given: {len(params)}, Requires: {max_params}')
-            elif len(params) == max_params:
-                print(f"OK, {self.cursor}{in_var}")
-                action(*params)  # Execute the action with the provided parameters
-            elif len(params) < max_params:
-                print(f'ERROR, {self.cursor}{in_var} -> Missing parameters. Given: {len(params)}, Requires: {max_params}')  # Respond if the command is not recognized
+                if len(params) > max_params:
+                    print(f'ERROR, {self.cursor}{in_var} -> Too many parameters. Given: {len(params)}, Requires: {max_params}')
+                elif len(params) == max_params:
+                    print(f"OK, {self.cursor}{in_var}")
+                    action(*params)  # Execute the action with the provided parameters
+                elif len(params) < max_params:
+                    print(f'ERROR, {self.cursor}{in_var} -> Missing parameters. Given: {len(params)}, Requires: {max_params}')  # Respond if the command is not recognized
+                else:
+                    print(f'ERROR, {self.cursor}{in_var} -> Unknown error ')  # Respond if
+            elif cmd == "help":
+                self._print_help()
             else:
-                print(f'ERROR, {self.cursor}{in_var} -> Unknown error ')  # Respond if
-        elif cmd == "help":
-            self._print_help()
-        else:
-            print(f'ERROR, {self.cursor}{in_var} Unknown command. Use "help" for supported commands ')  # Respond if the command is not recognized
+                print(f'ERROR, {self.cursor}{in_var} Unknown command. Use "help" for supported commands ')
+                # Respond if the command is not recognized
     def _print_help(self, cmd=""):
         cmd_keys = list(self.commands.keys())
         n=30
-        print("#"*n, "HELP", "#"*n)
+        print("-"*n, "HELP", "-"*n)
         for cmd in cmd_keys:
             action, params, help = self.commands[cmd]
-            print(f"{self.cursor} {cmd} params:{params}, Desc: {help}")
-        print("#" * n, "HELP", "#" * n)
+            print(f"{self.cursor} {cmd}  {'X '*params} ; Desc: {help}")
+        print("-" * n, "-" * 4, "-" * n)
+
 
