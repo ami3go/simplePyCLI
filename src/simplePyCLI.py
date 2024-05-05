c50fed13f74c7feffc2217b815e53b65868c61aa
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2024-05-05 20:47:21 +0300

    code format using black

--- src/simplePyCLI.py
+++ src/simplePyCLI.py
@@ -1,4 +1,3 @@
-
 """
 File: simplePyCLI.py
 Author: Aleksandr Chasnyk,
@@ -11,24 +10,29 @@ Simple CLI for micropython and python projects when
 command line interface is required.
 Tested on RP2040 MicroPython
 """
-class simplePyCLI:
 
+
+class simplePyCLI:
     def __init__(self, cli_symbol: str = "~"):
-        self.commands = {}  # Dictionary to store commands and their corresponding actions
+        self.commands = (
+            {}
+        )  # Dictionary to store commands and their corresponding actions
         self._cursor = cli_symbol
-        self._debug = False
+        self._debug = True
         self._error_msg = "ERROR"
         self._ok_msg = "OK"
         # add default command
         self.add_command("help", self._print_help, 0, "Print help")
-        self.add_command("toggle_debug", self._toggle_debug, 0, "Toggle short/long replay messages")
+        self.add_command(
+            "toggle_debug", self._toggle_debug, 0, "Toggle short/long replay messages"
+        )
 
     @property
     def cursor(self) -> None:
         return self._cursor
 
     @cursor.setter
-    def cursor(self, symbol: str) ->None:
+    def cursor(self, symbol: str) -> None:
         self._cursor = symbol
 
     @property
@@ -37,11 +41,11 @@ class simplePyCLI:
 
     @debug.setter
     def debug(self, val: bool) -> None:
-        '''
+        """
         Enable/Disable extended error messages
         :param val: bool could be  True or False
         :return: None
-        '''
+        """
         self._debug = bool(val)
 
     @property
@@ -50,11 +54,11 @@ class simplePyCLI:
 
     @error_msg.setter
     def error_msg(self, txt: str) -> None:
-        '''
+        """
         Setting short message in case of Error
         :param txt: string with short message for error, typically "ERROR"
         :return: None
-        '''
+        """
         self._error_msg = f"{txt}"
 
     @property
@@ -63,25 +67,31 @@ class simplePyCLI:
 
     @ok_msg.setter
     def ok_msg(self, txt: str) -> None:
-        '''
+        """
         Setting short confirmation message.
         It confirms that command founded in list
         and proper number of parameters provided
         :param txt: string with short message for error, typically "OK"
         :return: None
-        '''
+        """
         self._ok_msg = f"{txt}"
 
-    def add_command(self, command: str, action, max_params: int = 3, description: str = ""):
-        '''
+    def add_command(
+        self, command: str, action, max_params: int = 3, description: str = ""
+    ):
+        """
         Adding command into memory
         :param command: string that will bre recognised as command
         :param action: function which will be called
         :param max_params:  maximum number of parameters function need to take
         :param description: text description for generating help
         :return: None
-        '''
-        self.commands[command] = (action, max_params, description)  # Store the action and maximum parameters for the command
+        """
+        self.commands[command] = (
+            action,
+            max_params,
+            description,
+        )  # Store the action and maximum parameters for the command
         # print(self.commands)
 
     def process_command(self, cmd_with_params):
@@ -92,12 +102,12 @@ class simplePyCLI:
             if cmd in self.commands:
                 # Retrieve the action and maximum parameters for the command
                 action, max_params, help = self.commands[cmd]
-                #print(in_var, "given:", len(params), "max:", max_params)
+                # print(in_var, "given:", len(params), "max:", max_params)
                 if len(params) != max_params:
                     if len(params) > max_params:
-                        error_debug = f'{self._cursor}{in_var} -> Too many parameters. Given: {len(params)}, Requires: {max_params}'
+                        error_debug = f"{self._cursor}{in_var} -> Too many parameters. Given: {len(params)}, Requires: {max_params}"
                     else:
-                        error_debug = f'{self._cursor}{in_var} -> Missing parameters. Given: {len(params)}, Requires: {max_params}'
+                        error_debug = f"{self._cursor}{in_var} -> Missing parameters. Given: {len(params)}, Requires: {max_params}"
                     self._print_debug_msg(self._error_msg, error_debug)
                 else:
                     ok_debug = f"{self._cursor}{in_var}"
@@ -114,23 +124,22 @@ class simplePyCLI:
                 error_debug = f'{self._cursor}{in_var} Unknown command. Use "help" to get supported commands list'
                 self._print_debug_msg(self._error_msg, error_debug)
 
-
     def _toggle_debug(self) -> None:
-        '''
+        """
         Toggling debug variable value. you can see incoming messages
         :return: None
-        '''
+        """
         self.debug = not self.debug
 
-    def _print_debug_msg(self, short: str ="ERROR", long: str ="") -> None:
-        '''
+    def _print_debug_msg(self, short: str = "ERROR", long: str = "") -> None:
+        """
         Print "error" or "Ok" messages based on value of debug variable
         if debug = True full message will be returned
         if debug  False only shor message will be returned
         :param short: string for short message
         :param long: string for log messages
         :return: None
-        '''
+        """
         if self._debug:
             print(f"{short}, {long}")
         else:
@@ -143,4 +152,4 @@ class simplePyCLI:
         for cmd in cmd_keys:
             action, params, help = self.commands[cmd]
             print(f"{self._cursor} {cmd}  {'X ' * params} ; Desc: {help}")
-        print("-" * (2*n + 6))
+        print("-" * (2 * n + 6))
